8376838562c048a822cb1ca1741f698e10e0d7ef
arkhaledwebdev <arkhaledwebdev@gmail.com>
2023-11-23 20:17:59 +0200

    Finish

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import _ from 'lodash';
-import  './style.css';
-import Icon from './icon.png';
-
-// import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
   
     //  Lodash is now imported by this script
      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-     element.classList.add('hello');
 
-    //element.innerHTML = myName('Cody');
-
-    const myIcon = new Image();
-
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-  
     return element;
+
   }
   
   document.body.appendChild(component());
